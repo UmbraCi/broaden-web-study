33538b309eaad452eac3636f03e16b877564d03c
UmbraCi <UmbraCi>
2022-02-16 18:26:21 +0800

    用TS新建一个VUE项目

---
+++ part3/vue-ts-test/src/components/Foo.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1>Foo{{ msg }}</h1>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+// import Component from 'vue-class-component';
+//常规方式
+// export default {
+//     created:123
+// }
+
+//推荐：Options APIs
+export default Vue.extend({
+    name: 'Foo',
+    data() {
+        return {
+            msg: 'Options APIs',
+        };
+    },
+    // created(){
+
+    // }
+});
+
+//Class APIs
+// @Component
+// export default class Foo extends Vue{
+//     msg = "123"
+// }
+</script>
