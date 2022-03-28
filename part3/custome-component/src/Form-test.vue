4ef833aec27736ad03a0c3743e635a6e42f6ff25
UmbraCi <UmbraCi>
2022-03-28 18:30:58 +0800

    自定义组件模板

--- part3/custome-component/src/Form-test.vue
+++ part3/custome-component/src/Form-test.vue
@@ -6,14 +6,35 @@
         <lg-form-item label="密码" prop="password">
             <lg-input type="password" v-model="user.password"></lg-input>
         </lg-form-item>
-        <lg-form-item label="用户名" prop="username">
+        <lg-form-item>
             <lg-button type="primary" @click="login">登录</lg-button>
         </lg-form-item>
     </lg-form>
 </template>
 
 <script>
+import LgForm from './form/Form.vue'
+import lgFormItem from './form/FormItem'
+import LgInput from './form/Input'
+import lgButton from './form/Button'
 export default {
-    
+    components:{
+        LgForm,
+        lgFormItem,
+        LgInput,
+        lgButton
+    },
+    data() {
+        return {
+            user:{
+                username:'',
+                password:''
+            },
+            rules:{},
+        }
+    },
+    methods: {
+        login(){}
+    },
 }
 </script>
---
+++ part3/custome-component/src/form/FormItem.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <label for="">{{label}}</label>
+        <div>
+            <slot></slot>
+            <p v-if="errMessage">{{errMessage}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'lgFormItem',
+    props:{
+        label:{
+            type:String,
+        },
+        prop:{
+            type:String
+        }
+    },
+    data() {
+        return {
+            errMessage:''
+        }
+    },
+}
+</script>
