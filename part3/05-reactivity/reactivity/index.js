a1eca1ee16526a8db02ea8838fb93464fa206042
UmbraCi <umbraci@qq.com>
2022-04-18 18:29:09 +0800

    VUE3响应式原理

--- part3/05-reactivity/reactivity/index.js
+++ part3/05-reactivity/reactivity/index.js
@@ -8,7 +8,8 @@ export function reactive(target){
     const handler = {
         get(target, key, receiver){
             //收集依赖
-            console.log('get',key)
+            // console.log('get',key)
+            track(target,key)
             const result = Reflect.get(target, key, receiver)
             //递归调用
             return convert(result)
@@ -20,7 +21,8 @@ export function reactive(target){
                 //新旧值不相同
                 result = Reflect.set(target, key, value, receiver)
                 //触发更新
-                console.log('set',key,value)
+                // console.log('set',key,value)
+                trigger(target, key) 
             }
             return result
         },
@@ -29,10 +31,93 @@ export function reactive(target){
             const result = Reflect.deleteProperty(target, key)
             if(hadKey && result){
                 //触发更新
-                console.log('delete',key)
+                // console.log('delete',key)
+                trigger(target, key)
             }
             return result
         }
     }
     return new Proxy(target, handler)
+}
+
+let activeEffect = null
+let targetMap = new WeakMap()
+export function effect(callback){
+    activeEffect = callback
+    callback()  //访问响应式对象属性，去收集依赖
+    activeEffect = null
+}
+
+//收集依赖
+export function track(target,key){
+    if(!activeEffect)   return
+    let depsMap = targetMap.get(target)
+    if(!depsMap){
+        targetMap.set(target,(depsMap = new Map()))
+    }
+    let dep = depsMap.get(key)
+    if(!dep){
+        depsMap.set(key,(dep = new Set()))
+    }
+    dep.add(activeEffect)
+}
+
+export function trigger(target,key){
+    const depsMap = targetMap.get(target)
+    if(!depsMap)    return
+    const dep = depsMap.get(key)
+    if(!dep)    return
+    dep.forEach(effect => {
+        effect()
+    });
+}
+
+export function ref(raw){
+    //判断raw是否是ref创建的对象，如果是的话直接返回
+    if(isObject(raw) && raw.__v__isRef)     return
+    let value = convert(raw)
+    const r = {
+        __v__isRef:true,
+        get value(){
+            //收集依赖
+            track(r,'value')
+            return value
+        },
+        set value(newValue){
+            if(newValue !== value){
+                 raw = newValue
+                 //重新赋值还是响应式的
+                 value = convert(raw)
+                 trigger(r,'value')
+            }
+        }
+    }
+    return r
+}
+
+export function toRefs(proxy){
+    const ret = proxy instanceof Array ? new Array(proxy.length) : {}
+    for (const key in proxy) {
+        ret[key] = toProxyRef(proxy,key)
+    }
+    return ret 
+}
+
+function toProxyRef(proxy,key){
+    const r = {
+        __v__isRef:true,
+        get value(){
+            return proxy[key]
+        },
+        set value(newValue){
+            proxy[key] = newValue
+        }
+    }
+    return r
+}
+
+export function computed(getter){
+    const result = ref()
+    effect(()=> (result.value = getter()))
+    return result
 }
